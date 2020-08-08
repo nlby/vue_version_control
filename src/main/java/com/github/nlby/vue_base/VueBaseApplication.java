8121535bd3e2db6939b298cd5898b1550a177403
nlby <1029253541@qq.com>
2020-08-08 12:00:35 +0800

    first commit

---
+++ src/main/java/com/github/nlby/vue_base/VueBaseApplication.java
@@ -0,0 +1,13 @@
+package com.github.nlby.vue_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/nlby/vue_base/VueBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nlby.vue_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
